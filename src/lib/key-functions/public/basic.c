4b9da3d58da9fe412871607d1a32da9c6bc4608b
silverthornne <silverthornne@gmail.com>
2014-12-18 17:50:05 -0400

    LED Changes reverted.
    Sorry, I thought I was working on my fork. I removed the LED changes.

--- src/lib/key-functions/public/basic.c
+++ src/lib/key-functions/public/basic.c
@@ -27,7 +27,6 @@
 #define  WAS_PRESSED   main_arg_was_pressed
 
 // ----------------------------------------------------------------------------
-extern uint8_t keyboard_leds;
 /*
  * [name]
  *   Press|Release
@@ -169,7 +168,6 @@ static void layer_pop(uint8_t local_id) {
  *   the top of the stack, and record the id of that layer element
  */
 void kbfun_layer_push_1(void) {
-	keyboard_leds = keyboard_leds | 0x02;
 	layer_push(1);
 }
 
@@ -222,7 +220,6 @@ void kbfun_layer_sticky_1  (void) {
  *   touching any other elements)
  */
 void kbfun_layer_pop_1(void) {
-	keyboard_leds = keyboard_leds &amp; ~(uint8_t)0x02;
 	layer_pop(1);
 }
 
@@ -235,7 +232,6 @@ void kbfun_layer_pop_1(void) {
  *   the top of the stack, and record the id of that layer element
  */
 void kbfun_layer_push_2(void) {
-	keyboard_leds = keyboard_leds | 0x01;
 	layer_push(2);
 }
 
@@ -260,7 +256,6 @@ void kbfun_layer_sticky_2  (void) {
  *   touching any other elements)
  */
 void kbfun_layer_pop_2(void) {
-	keyboard_leds = keyboard_leds &amp; ~(uint8_t)0x01;
 	layer_pop(2);
 }
 
